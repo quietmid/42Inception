cfa604b327b8e3674d0083aad581142c10fe9a39
quietmid <ninjonglu@gmail.com>
2025-06-11 15:55:14 +0300

    failed so updated with notes

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 set -e
-
+# -e if a command fails, the script stop
 /usr/local/bin/wp-install.sh
 
 echo "Starting PHP-FPM..."
+# -F foreground mode. php is a specific implementation of PHP that's optimized for web serv
 exec php-fpm83 -F
